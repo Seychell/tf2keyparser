bb8dd7cce59d825343804024591b13d617bb0c24
seychell <noprivate@mail.ru>
2023-03-01 13:34:22 +0300

    changed the tf2_key_steam string formatting

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def send_prices():
     exchange_url = 'https://steamcommunity.com/market/priceoverview/?appid=440&country=russia&currency=5&market_hash_name=Mann+Co.+Supply+Crate+Key'
     exchange_response = requests.get(exchange_url)
     exchange_data = exchange_response.json()
-    tf2_key_steam_string = exchange_data["lowest_price"][:6].replace(',', '.')
+    tf2_key_steam_string = re.sub('[^0-9\.]','',exchange_data["lowest_price"][:6].replace(',', '.'))
     tf2_key_steam_float = round(float(tf2_key_steam_string) * 0.868,2)
     for chat_id in chat_ids:
         await bot.send_message(chat_id=chat_id, text=f"{tf2_key_steam_float} steam\n{tf2_key_lavka_float} tf2lavka")
